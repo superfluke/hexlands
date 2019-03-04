7894277081e1945ebe713c2086b69437ec399e66
Unknown <anon@mail.com>
2019-03-03 22:13:19 -0500

    Add separate world time in hex dim. Update forge

--- src/main/java/fluke/hexlands/util/Reference.java
+++ src/main/java/fluke/hexlands/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "hexlands";
 	public static final String NAME = "Hex Lands";
-	public static final String VERSION = "1.8.1";
+	public static final String VERSION = "1.9.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "fluke.hexlands.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "fluke.hexlands.proxy.CommonProxy";
--- src/main/java/fluke/hexlands/world/WorldProviderHex.java
+++ src/main/java/fluke/hexlands/world/WorldProviderHex.java
@@ -1,17 +1,27 @@
 package fluke.hexlands.world;
 
 import fluke.hexlands.Main;
+import fluke.hexlands.config.Configs;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.WorldProvider.WorldSleepResult;
+import net.minecraft.world.end.DragonFightManager;
 import net.minecraft.world.gen.IChunkGenerator;
 
 public class WorldProviderHex extends WorldProvider
 {
+	private long worldTime = 0;
+	
 	@Override
 	protected void init()
     {
         this.hasSkyLight = true;
         this.biomeProvider =  new HexBiomeProvider(world.getWorldInfo());
+        NBTTagCompound nbttagcompound = this.world.getWorldInfo().getDimensionData(Configs.dimension.dimID);
+        this.worldTime = this.world instanceof WorldServer ? nbttagcompound.getLong("HexTime") : 0;
     }
 	
 	
@@ -38,6 +48,30 @@ public class WorldProviderHex extends WorldProvider
         return true;
     }
 	
+	@Override
+	public WorldSleepResult canSleepAt(net.minecraft.entity.player.EntityPlayer player, BlockPos pos)
+    {
+        return WorldSleepResult.ALLOW;
+    }
 	
+	@Override
+	public long getWorldTime()
+    {
+        return this.worldTime;
+    }
+	
+	@Override
+	public void setWorldTime(long time)
+    {
+        this.worldTime = time;
+    }
+	
+	@Override
+	public void onWorldSave()
+    {
+        NBTTagCompound nbttagcompound = new NBTTagCompound();
+        nbttagcompound.setLong("HexTime", this.worldTime);
+        this.world.getWorldInfo().setDimensionData(Configs.dimension.dimID, nbttagcompound);
+    }
 
 }
